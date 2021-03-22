2b0720cdb48f02e8a6c66c4be4214fdeeff8ef42
SomodiTeodor <somodi.teodor@gmail.com>
2021-03-23 01:53:46 +0200

    (feature/initial-setup): Move main project to its own folder. Add Test project.

---
+++ ExperBE.Tests/TestsConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExperBE.Models.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExperBE.Tests
+{
+    [TestClass]
+    public class TestsConfiguration
+    {
+        [AssemblyInitialize]
+        public void TestsGlobalInitialize()
+        {
+            ExperConfiguration.ConnectionString = "";
+            ExperConfiguration.JwtSecret = "TestSecret";
+            ExperConfiguration.SendEmails = false;
+        }
+    }
+}
